85e2ab74d78e4e7ad39d52af8a8ae9d43da8d3f7
Jason Shockness <jshock12@gmail.com>
2019-07-17 05:11:08 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,21 +1,20 @@
+# book.rb
+
 class Book
+  attr_accessor :author, :page_count, :genre 
+  attr_reader :title
   
   def initialize(title)
     @title = title
   end
   
-  def title
-    @title
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast"
   end
   
-  def author= (author)
-    @author = author
-  end
+end
   
-  def author
-    @author
-  end
   
-  def page_count = (num)
    
 
