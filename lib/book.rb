220f637ecc3917ad4eeb5ff391ccec8ec3a8ee7b
Jason Shockness <jshock12@gmail.com>
2019-07-17 04:49:18 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,21 @@
 class Book
-end
-
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  def author= (author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count = (num)
+   
 
